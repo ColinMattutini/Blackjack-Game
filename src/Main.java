1442c7835114ed809025924da4f3b0358e50f1ca
Colin <cmmatt14@gmail.com>
2022-08-09 20:39:17 -0400

    Updated to create player object and allow player to keep playing

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
        BlackJackMainGame play = new BlackJackMainGame();
-       play.start();
+       Player player = new Player("Player");
+       play.startMessage();
+       do {
+           play.start(player);
+       }while(play.playAgain(player) == true);
+
+
 
     }
 }
